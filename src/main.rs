7f3df338abd6a4afd59be3ee31adea69135c914c
Jamie <yames.git@gmail.com>
2025-08-01 23:42:39 -0400

    added cors layer

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@ mod models;
 mod tasks;
 mod routes;
 
+use axum::http::Method;
 use std::net::SocketAddr;
 use tower_http::trace::{TraceLayer, DefaultMakeSpan, DefaultOnResponse};
+use tower_http::cors::{CorsLayer, Any};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
@@ -20,11 +22,17 @@ async fn main() {
     let app_state = state::AppState::new().await.expect("Failed to init DB");
     tasks::cleanup::start_cleanup_task(app_state.db.clone()).await;
 
-    let app = routes::create_router(app_state).layer(
-        TraceLayer::new_for_http()
-            .make_span_with(DefaultMakeSpan::new().include_headers(true))
-            .on_response(DefaultOnResponse::new().include_headers(true)),
-    );
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(Any);
+
+    let trace = TraceLayer::new_for_http()
+        .make_span_with(DefaultMakeSpan::new().include_headers(true))
+        .on_response(DefaultOnResponse::new().include_headers(true));
+
+    let app = routes::create_router(app_state)
+        .layer(cors)
+        .layer(trace);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::info!("Listening on http://{}", addr);

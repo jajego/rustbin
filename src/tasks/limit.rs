c00de194df7ecb361dfa7ec961897a4fc408b133
Jamie <yames.git@gmail.com>
2025-08-02 18:09:25 -0400

    adjustment

---
+++ src/tasks/limit.rs
@@ -0,0 +1,18 @@
+use std::sync::Arc;
+use std::time::Duration;
+use tower_governor::{governor::GovernorConfig};
+use tower_governor::key_extractor::PeerIpKeyExtractor;
+use governor::middleware::NoOpMiddleware;
+use governor::clock::QuantaInstant;
+
+pub async fn start_rate_limit_cleanup(conf: &Arc<GovernorConfig<PeerIpKeyExtractor, NoOpMiddleware<QuantaInstant>>>) {
+   let governor_limiter = conf.limiter().clone();
+   let interval = Duration::from_secs(60);
+   std::thread::spawn(move || {
+       loop {
+           std::thread::sleep(interval);
+           tracing::info!("rate limiting storage size: {}", governor_limiter.len());
+           governor_limiter.retain_recent();
+       }
+   });
+}
